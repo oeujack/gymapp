504b6941d709661274483c794f4de321136a4bda
Jackson dos Santos <jackson1santos@proton.me>
2025-04-15 21:07:12 -0300

    feat: customizando o tema padrao

--- App.tsx
+++ App.tsx
@@ -3,29 +3,29 @@ import {
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto'
-import { GluestackUIProvider, Text } from '@gluestack-ui/themed'
-import { StatusBar, View } from 'react-native'
+import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed'
+import { StatusBar } from 'react-native'
+import { config } from './config/gluestack-ui.config'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
 
   return (
-    <GluestackUIProvider>
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: '#202024',
-        }}
-      >
-        <StatusBar
-          barStyle="light-content"
-          translucent
-          backgroundColor="transparent"
-        />
-        {fontsLoaded ? <Text color="white" fontSize={32}>Home</Text> : <></>}
-      </View>
+    <GluestackUIProvider config={config}>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      {fontsLoaded ? (
+        <Center flex={1} bgColor="$gray700">
+          <Text color="white" fontSize={32}>
+            Home
+          </Text>
+        </Center>
+      ) : (
+        <></>
+      )}
     </GluestackUIProvider>
   )
 }

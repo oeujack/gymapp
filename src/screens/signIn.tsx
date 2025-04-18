a6e259caaab8c7b66e244f3bc567c8aa87c7a679
Jackson dos Santos <jackson1santos@proton.me>
2025-04-18 11:59:45 -0300

    style: implementa propriedade variant para alterar estilos do button

--- src/screens/signIn.tsx
+++ src/screens/signIn.tsx
@@ -16,9 +16,11 @@ export function SignIn() {
         alt="Pessoas treinando"
         position="absolute"
       />
+
       <VStack flex={1} px="$10" pb="$16">
         <Center my="$24">
           <Logo />
+
           <Text color="$gray100" fontSize="$sm">
             Treine sua mente e o seu corpo.
           </Text>
@@ -26,13 +28,23 @@ export function SignIn() {
 
         <Center gap="$2">
           <Heading color="$gray100">Acesse a conta</Heading>
+
           <Input
             placeholder="E-mail"
             keyboardType="email-address"
             autoCapitalize="none"
           />
           <Input placeholder="Senha" secureTextEntry />
+
           <Button title="Acessar" />
+        </Center>
+
+        <Center flex={1} justifyContent="flex-end" marginTop="$4">
+          <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
+            Ainda não tem acesso?
+          </Text>
+
+          <Button title="Criar conta" variant="outline" />
         </Center>
       </VStack>
     </VStack>

8c63c447de34818d4ccfbd939c48b5c2fe2ef3ac
Jackson dos Santos <jackson1santos@proton.me>
2025-04-18 20:44:27 -0300

    feat: tipa as rotas do app

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,14 +1,26 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import {
+  createBottomTabNavigator,
+  BottomTabNavigationProp,
+} from '@react-navigation/bottom-tabs'
 import { Home } from '@screens/Home'
 import { History } from '@screens/History'
 import { Profile } from '@screens/Profile'
 import { Exercise } from '@screens/Exercise'
 
-const { Navigator, Screen } = createBottomTabNavigator()
+type AppRoutesProps = {
+  home: undefined
+  exercise: undefined
+  profile: undefined
+  history: undefined
+}
+
+export type AppNavigatorRouterProps = BottomTabNavigationProp<AppRoutesProps>
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutesProps>()
 
 export function AppRoutes() {
   return (
-    <Navigator>
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
       <Screen name="history" component={History} />
       <Screen name="profile" component={Profile} />

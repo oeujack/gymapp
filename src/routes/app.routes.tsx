0fd5dd5fc7bf8952b8b5b53afc5ed9cc16e8eb21
Jackson dos Santos <jackson1santos@proton.me>
2025-04-18 21:12:51 -0300

    feat: estiliza a Bottom Navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -11,6 +11,7 @@ import { gluestackUIConfig } from '../../config/gluestack-ui.config'
 import HomeSvg from '@assets/home.svg'
 import HistorySvg from '@assets/history.svg'
 import ProfileSvg from '@assets/profile.svg'
+import { Platform } from 'react-native'
 
 type AppRoutesProps = {
   home: undefined
@@ -27,7 +28,22 @@ export function AppRoutes() {
   const { tokens } = gluestackUIConfig
   const iconSize = tokens.space['6']
   return (
-    <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: tokens.colors.green500,
+        tabBarInactiveTintColor: tokens.colors.gray200,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          backgroundColor: tokens.colors.gray600,
+          borderTopWidth: 0,
+          height: Platform.OS === 'android' ? 'auto' : 96,
+          paddingBottom: tokens.space['10'],
+          paddingTop: tokens.space['6'],
+        },
+      }}
+    >
       <Screen
         name="home"
         component={Home}
@@ -55,7 +71,16 @@ export function AppRoutes() {
           ),
         }}
       />
-      <Screen name="exercise" component={Exercise} />
+      <Screen
+        name="exercise"
+        component={Exercise}
+        options={{
+          tabBarButton: () => null,
+          tabBarItemStyle: {
+            display: 'none',
+          },
+        }}
+      />
     </Navigator>
   )
 }

152dcd5ddeaecdb08414db8607a107cd5baee617
Jackson dos Santos <jackson1santos@proton.me>
2025-05-20 22:45:26 -0300

    feat: implementa hooks

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -2,7 +2,7 @@ import { DefaultTheme, NavigationContainer } from "@react-navigation/native"
 import { useContext } from "react"
 import { gluestackUIConfig } from "../../config/gluestack-ui.config"
 import { Box } from "@gluestack-ui/themed"
-import { AppRoutes } from "./app.routes"
+import { useAuth } from "@hooks/useAuth"
 import { AuthRoutes } from "./auth.routes"
 import { AuthContext } from "@contexts/AuthContext"
 
@@ -10,9 +10,9 @@ export function Routes() {
   const theme = DefaultTheme
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
 
-  const contextData = useContext(AuthContext)
+  const { user } = useAuth()
 
-  console.log("USUÁRIO LOGADO =>", contextData.user.name)
+  console.log("USUÁRIO LOGADO =>", user.name)
 
   return (
     <Box flex={1} bg="$gray700">

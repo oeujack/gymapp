f231e29df10470bb5285285981d8dcade5828f76
Jackson dos Santos <jackson1santos@proton.me>
2025-06-25 19:04:12 -0300

    feat: exibe exericios por grupo

--- mobile/src/components/ExerciseCard/index.tsx
+++ mobile/src/components/ExerciseCard/index.tsx
@@ -1,3 +1,4 @@
+import type { Exercise } from '@dtos/Exercise'
 import {
   Text,
   Heading,
@@ -9,9 +10,12 @@ import {
 import { ChevronRight } from 'lucide-react-native'
 import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
 
-type Props = TouchableOpacityProps & {}
+type Props = TouchableOpacityProps & {
+  data: Exercise
 
-export function ExerciseCard({ ...props }: Props) {
+}
+
+export function ExerciseCard({ data, ...props }: Props) {
   return (
     <TouchableOpacity {...props}>
       <HStack
@@ -36,10 +40,10 @@ export function ExerciseCard({ ...props }: Props) {
 
         <VStack flex={1}>
           <Heading fontSize="$lg" color="$white" fontFamily="$heading">
-            Puxada frontal
+            {data.name}
           </Heading>
           <Text fontSize="$sm" color="$gray200" mt="$1" numberOfLines={2}>
-            3 séries x 12 repetições
+            {data.series} séries x {data.repetitions} repetições
           </Text>
         </VStack>
 
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -109,8 +109,8 @@ export function Home() {
         <FlatList
           data={exercises}
           keyExtractor={item => item.id}
-          renderItem={() => (
-            <ExerciseCard onPress={handleOpenExerciseDetails} />
+          renderItem={({ item }) => (
+            <ExerciseCard onPress={handleOpenExerciseDetails} data={item}/>
           )}
           showsVerticalScrollIndicator={true}
           contentContainerStyle={{ paddingBottom: 20 }}
